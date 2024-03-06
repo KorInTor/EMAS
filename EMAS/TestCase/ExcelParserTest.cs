7d59c7ea41793d4cf3dfd70bd2684b846e382d22
Danil Pryahin <pradanilhin@gmail.com>
2024-03-06 23:07:25 +0700

    feature: - New Constructor in Equipment that allows to create object with all parametrs. - New Package ClosedXML, allows to easily import and export data to and from Excel. - ExcelParser Class, Allows Import and Export data to/from Excel. - ExcelParserTest Class, Tests ExcelParser (in future can be used with MSTest).

---
+++ EMAS/TestCase/ExcelParserTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using EMAS.Model;
+using EMAS.Service.Serialization;
+using System.IO;
+
+namespace EMAS.TestCases
+{
+    //[TestClass] - Задел на будущие тесты используя MSTest.
+    public class ExcelParserTest
+    {
+        private static Equipment exampleEquipment = new("Списан", "REG123", "Описание", "1", "°C", "0…10", 1, "Марка", "Производитель", "Тип", "FN123", ["Tag1", "Tag2"]);
+
+        private static string inputRelativePath = @"..\..\..\Examples\InputExcelExample.xlsx";
+
+        private static string inputFullPath = Path.GetFullPath(inputRelativePath);
+
+        private static string outputRelativePath = @"..\..\..\Examples\OutputExcelExample.xlsx";
+
+        private static string outputFullPath = Path.GetFullPath(outputRelativePath);
+
+        //[TestMethod]
+        public void EquipmentInputTest()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("Excel_Result.txt"));
+            Trace.AutoFlush = true;
+
+            //var expected = exampleEquipment;
+            
+            //var actual = ExcelParser.ImportFromExcel(inputFullPath)[0];
+
+            //Assert.AreEqual(expected, actual);
+
+            List<Equipment> equipments = ExcelParser.ImportFromExcel(inputFullPath);
+
+            Trace.WriteLine($"Результат считывания из Excel файла: {equipments[0].Equals(exampleEquipment)}");
+        }
+
+        //[TestMethod]
+        public void EquipmentOutputTest()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("Excel_Result.txt"));
+            Trace.AutoFlush = true;
+
+            //var expected = exampleEquipment;
+
+            //var actual = ExcelParser.ImportFromExcel(outputFullPath)[0];
+
+            //Assert.AreEqual(expected, actual);
+
+            ExcelParser.ExportToExcel([exampleEquipment],outputFullPath);
+
+            Trace.WriteLine($"Результат экспорта в Excel файл: {exampleEquipment.Equals(ExcelParser.ImportFromExcel(outputFullPath)[0])}");
+        }
+
+        //[TestMethod]
+        public void EquipmentInputOutputTest()
+        {
+            EquipmentInputTest();
+            EquipmentOutputTest();
+        }
+    }
+}
