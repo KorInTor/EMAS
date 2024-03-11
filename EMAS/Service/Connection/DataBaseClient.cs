19b57229c0e5375b3ffbd8a22c7e3439945fdf6b
Danil Pryahin <pradanilhin@gmail.com>
2024-03-11 21:14:49 +0700

    feature: - GetAllEquipmentData() - Returns equipment data from all locations.

--- EMAS/Service/Connection/DataBaseClient.cs
+++ EMAS/Service/Connection/DataBaseClient.cs
@@ -258,6 +258,35 @@ namespace EMAS.Service.Connection
         {
             throw new NotImplementedException();
         }
+        
+        public static List<Equipment> GetAllEquipmentData()
+        {
+            using var connection = new NpgsqlConnection(ConnectionString);
+            connection.Open();
+
+            string sql = "SELECT id, \"name\", manufacturer, \"type\", measurment_units, accuracy_class, measurment_limit, serial_number, inventory_number, tags, location_id, status, description FROM public.equipment;";
+
+            using var command = new NpgsqlCommand(sql, connection);
+
+            var list = new List<Equipment>();
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    List<string> tags = [.. ((string[])reader[9])]; // Считывание tags
+                    list.Add(new Equipment(reader.GetString(11), reader.GetString(8), reader.GetString(12), reader.GetString(5), reader.GetString(4), reader.GetString(6), reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(7), tags));
+                }
+
+                foreach (var data in list)
+                {
+                    Debug.WriteLine($"Получено оборудование: {data.Id} - {data.Name}");
+                }
+
+            }
+            connection.Close();
+            return list;
+        }
 
         public static List<Employee> GetAllEmployeeData()
         {
@@ -518,5 +547,7 @@ namespace EMAS.Service.Connection
         {
             throw new NotImplementedException();
         }
+
+        
     }
 }
