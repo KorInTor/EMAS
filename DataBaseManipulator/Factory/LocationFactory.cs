7a79fde012c3db492f7afb7be0ccb691cb8e4d27
Danil Pryahin <pradanilhin@gmail.com>
2024-03-15 10:36:46 +0700

    feature: - New project for fast adding and cleaning database.

---
+++ DataBaseManipulator/Factory/LocationFactory.cs
@@ -0,0 +1,61 @@
+﻿using EMAS.Model;
+
+namespace DataBaseManipulator.Factory
+{
+    public static class LocationFactory
+    {
+        private static string[] Names =
+        {
+            "Тымпучиканский участок недр",
+            "Вакунайский участок",
+            "БНУ",
+            "Лукойл Пякяхинское",
+            "Семаковское месторождение",
+            "Чаяндинское НГКМ",
+            "Новопортовское ГКМ",
+            "Ачим Девелопмент",
+            "Заполярное НГКМ",
+            "ГДУ",
+            "ПКИОС 66",
+            "Бованенково",
+            "АчимГаз",
+            "ПКИОС 6",
+            "ПКИОС 2",
+            "ПКИОС 3",
+            "ПКИОС 5",
+            "ПКИОС 1",
+            "ПКИОС 39",
+            "БКМ № 0053",
+            "МЗС",
+            "ПКИОС 4",
+            "БК №1",
+            "БКМ № 2",
+            "БКМ2 № 3",
+            "БК2 №5",
+            "МТСУ № 250",
+            "МТСУ № 331",
+            "МТСУ № 487",
+            "МТСУ № 477",
+            "МТСУ № 478",
+            "МТСУ № 480",
+            "МУИС-1",
+            "УПВ с БКНС"
+        };
+
+        public static Location[] GetLocations(int numberOfLocations = 0)
+        {
+            if (numberOfLocations == 0)
+            {
+                numberOfLocations = Names.Length;
+            }
+
+            Location[] locations = new Location[numberOfLocations];
+            for (int i = 0; i < numberOfLocations; i++)
+            {
+                locations[i] = new Location(0, Names[i]);
+            }
+            return locations;
+        }
+
+    }
+}
---
+++ DataBaseManipulator/Program.cs
@@ -0,0 +1,37 @@
+﻿using EMAS.Model;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Choose table:");
+        Console.WriteLine("1 - Location.");
+        Console.WriteLine("2 - Employee.");
+        Console.WriteLine("3 - Equipment.");
+        Console.WriteLine("or:");
+        Console.WriteLine("C - Clear all Tables.");
+        char command;
+        command = Console.ReadKey().KeyChar;
+
+        switch (command)
+        {
+            case '1':
+                {
+                    break;
+                }
+            case '2':
+                {
+                    break;
+                }
+            case '3':
+                {
+                    break;
+                }
+            case 'C':
+                {
+                    break;
+                }
+        }
+            
+    }
+}
