d18a8a7970d0f9d0d4a6b5c988d16d1cbae5da7f
Danil Pryahin <pradanilhin@gmail.com>
2024-04-04 17:37:49 +0700

    feature Reservation: - Model Reservation;

---
+++ EMAS/Model/Reservation.cs
@@ -0,0 +1,68 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMAS.Model
+{
+    public class Reservation : ObservableObject
+    {
+        private long _id;
+
+        private Equipment _equipment;
+
+        private DateTime _startDate;
+
+        private Employee _reservedBy;
+
+        private string _additionalInfo;
+
+        public long Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+        public Equipment Equipment
+        {
+            get => _equipment;
+            set => SetProperty(ref _equipment, value);
+        }
+
+        public DateTime StartDate
+        {
+            get => _startDate;
+            set => SetProperty(ref _startDate, value);
+        }
+
+        public Employee ReservedBy
+        {
+            get => _reservedBy;
+            set => SetProperty(ref _reservedBy, value);
+        }
+
+        public string AdditionalInfo
+        {
+            get => _additionalInfo;
+            set => SetProperty(ref _additionalInfo, value);
+        }
+
+        public Reservation(long id, DateTime startDate, Employee reservedBy, string additionalInfo, Equipment equipment)
+        {
+            Id = id;
+            StartDate = startDate;
+            ReservedBy = reservedBy;
+            AdditionalInfo = additionalInfo;
+            Equipment = equipment;
+        }
+
+        public Reservation() 
+        {
+            StartDate = DateTime.MinValue;
+
+            Equipment = new();
+        }
+    }
+}
