45f01a09eee769ae6c6d578c24c836a1cbb0f4b7
Danil Pryahin <pradanilhin@gmail.com>
2024-04-04 17:01:23 +0700

    feature LocationPicker (VM,control,window): - LocationPickerVM - DataContext для LocationPicker, контрол которые инициализируется Dictionary<int,string> Locations, при выборе зажигает событие с аргументом = KeyValuePair выбранной локации. - LocationPickerWindow - Окно которое хранит LocationPickerControl. - Note - Подтверждение действий или провала нужно делать через метод в LocationPickerVM в который нужно передать успешно ли действие и сообщение на UI; Не зависимо от того был ли это успех или неудача будет вызвано MessageBox с Сообщением переданным в методе; - Если Control Находится в LocationPickerWindow то окно закроется если действие было успешно

---
+++ EMAS/Controls/LocationPickerControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using EMAS.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMAS.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для LocationPickerControl.xaml
+    /// </summary>
+    public partial class LocationPickerControl : UserControl
+    {
+        public LocationPickerControl()
+        {
+            InitializeComponent();
+            if (DataContext == null)
+            {
+                Debug.WriteLine($"DataContext не назанчен для{ nameof(LocationPickerControl)}");
+                return;
+            }
+            LocationPickerVM dataContext = DataContext as LocationPickerVM;
+            dataContext.ActionSuccessful += ShowSuccesfullMessage;
+            dataContext.ActionFailed += ShowFailedMessage;
+        }
+
+        private void ShowFailedMessage(string message)
+        {
+            MessageBox.Show(message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ShowSuccesfullMessage(string message)
+        {
+            MessageBox.Show(message, "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+    }
+}
---
+++ EMAS/Windows/Dialogue/LocationPickerWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using EMAS.Controls;
+using EMAS.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EMAS.Windows.Dialogue
+{
+    /// <summary>
+    /// Логика взаимодействия для LocationPickerWindow.xaml
+    /// </summary>
+    public partial class LocationPickerWindow : Window
+    {
+        public LocationPickerWindow()
+        {
+            InitializeComponent();
+            if (DataContext == null)
+            {
+                Debug.WriteLine($"DataContext не назанчен для{nameof(LocationPickerControl)}");
+                return;
+            }
+            LocationPickerVM dataContext = DataContext as LocationPickerVM;
+            dataContext.ActionSuccessful += CloseWindow;
+        }
+
+        private void CloseWindow(string obj)
+        {
+            this.Close();
+        }
+    }
+}
