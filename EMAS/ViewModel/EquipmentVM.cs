a725dc64aa895932abb788e8a7bc2a2d4e537570
Danil Pryahin <pradanilhin@gmail.com>
2024-04-09 02:58:25 +0700

    feature: - Permission Access У нас теперь есть получение разрешение из базы и составление контекстного меню в зависимости от разрешений.

--- EMAS/ViewModel/EquipmentVM.cs
+++ EMAS/ViewModel/EquipmentVM.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using EMAS.Model;
+using EMAS.Model.Enum;
 using EMAS.Service.Connection;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -23,10 +24,24 @@ namespace EMAS.ViewModel
         private RelayCommand _clearFiltersCommand;
 
         [ObservableProperty]
-        private RelayCommand _openHistoryWindowCommand;
+        [NotifyCanExecuteChangedFor(nameof(OpenAdditionWindow))]
+        private bool _canAdd;
 
         [ObservableProperty]
-        private RelayCommand _openAdditionWindow;
+        [NotifyCanExecuteChangedFor(nameof(OpenEditWindow))]
+        private bool _canEdit;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(OpenDeliveryWindow))]
+        private bool _canSend;
+
+        public RelayCommand OpenAdditionWindow { get; set; }
+        public RelayCommand OpenHistoryWindowCommand { get; set; }
+        public RelayCommand OpenEditWindow { get; set; }
+        public RelayCommand OpenDeliveryWindow { get; set; }
+
+        [ObservableProperty]
+        public Dictionary<string, RelayCommand> _namedCommands = [];
 
         [ObservableProperty]
         private Equipment _selectedEquipment;
@@ -41,14 +56,72 @@ namespace EMAS.ViewModel
         {
             ClearFiltersCommand = new RelayCommand(ClearFilters);
             OpenHistoryWindowCommand = new RelayCommand(RequestHistoryWindow);
-            OpenAdditionWindow = new RelayCommand(RequestAdditionWindow);
+            OpenAdditionWindow = new RelayCommand(RequestAdditionWindow, () => CanAdd);
+            OpenEditWindow = new RelayCommand(RequestEditWindow, () => CanEdit);
+            OpenDeliveryWindow = new RelayCommand(RequestDeliveryCreationWindow, () => CanSend);
 
+            InitCommandDictionary();
             DesiredEquipment.PropertyChanged += FilterEquipment;
+        }
+
+        private void InitCommandDictionary()
+        {
+            NamedCommands.Add("Добавить", OpenAdditionWindow);
+            NamedCommands.Add("Изменить", OpenEditWindow);
+            NamedCommands.Add("Отправить", OpenDeliveryWindow);
+        }
+
+        private void RequestDeliveryCreationWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RequestEditWindow()
+        {
+            throw new NotImplementedException();
         }
 
         partial void OnEquipmentSourceListChanged(List<Equipment> value)
         {
             FilterEquipment(this, new PropertyChangedEventArgs(nameof(EquipmentSourceList)));
+        }
+
+        public void ChangeCommandAccess(List<string> permissions)
+        {
+            CanAdd = false;
+            CanEdit = false;
+            CanSend = false;
+            Dictionary<string,RelayCommand> newNamedCommandList = [];
+
+            foreach(string permission in permissions)
+            {
+                if (Enum.TryParse(permission, out PermissionType permissionType))
+                {
+                    switch (permissionType)
+                    {
+                        case PermissionType.EquipmentAdd:
+                        {
+                            CanAdd = true;
+                                newNamedCommandList.Add("Добавить", OpenAdditionWindow);
+                            break;
+                        }
+                        case PermissionType.EquipmentEdit:
+                        {
+                            CanEdit = true;
+                                newNamedCommandList.Add("Изменить", OpenEditWindow);
+                            break;
+                        }
+                        case PermissionType.DeliveryAccess:
+                        {
+                            CanSend = true;
+                                newNamedCommandList.Add("Отправить", OpenDeliveryWindow);
+                            break;
+                        }
+                    }
+                }
+            }
+            newNamedCommandList.Add("Просмотреть историю", OpenHistoryWindowCommand);
+            NamedCommands = newNamedCommandList;
         }
 
         private void FilterEquipment(object? sender, PropertyChangedEventArgs e)
