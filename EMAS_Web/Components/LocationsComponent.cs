09040145333535aeaeac2f62087b93a9ec596322
Danil Pryahin <pradanilhin@gmail.com>
2024-09-19 15:53:40 +0700

    feature locationSwitcher - dynamicly witches location of current page if can - new design for button - if cant switch current location then use old logic for making button change href for top bar links

--- EMAS_Web/Components/LocationsComponent.cs
+++ EMAS_Web/Components/LocationsComponent.cs
@@ -7,16 +7,10 @@ namespace EMAS_Web.Components
     [ViewComponent]
     public class LocationsComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(int? currentLocationId)
         {
-            List<string> locations = [];
-            foreach(Location loc in DataBaseClient.GetInstance().SelectLocations())
-            {
-                locations.Add(loc.Name);
-
-            }
-           
-            return View("LocationSwitch", locations);
+            ViewBag.CurrentLocationId = currentLocationId;
+            return View("LocationSwitch", DataBaseClient.GetInstance().SelectNamedLocations());
         }
     }
 }
