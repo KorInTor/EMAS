f078070679641064369a584f4f84637dfe8f8cc7
Danil Pryahin <pradanilhin@gmail.com>
2024-09-22 16:55:28 +0700

    refactor ElapsedTimeFormater: - now Uses fewer space.

--- Service/ElapsedTimeFormatter.cs
+++ Service/ElapsedTimeFormatter.cs
@@ -13,56 +13,41 @@ namespace Service
 			DateTime now = DateTime.Now;
 			TimeSpan timeSpan = now - pastDate;
 
-			if (timeSpan.TotalDays >= 365)
-			{
-				int years = (int)(timeSpan.TotalDays / 365);
-				return $"Прошло {years} {GetPluralForm(years, "год", "года", "лет")}";
-			}
-			else if (timeSpan.TotalDays >= 30)
+			//if (timeSpan.TotalDays >= 365)
+			//{
+			//	int years = (int)(timeSpan.TotalDays / 365);
+			//	return $"{years} г.";
+			//}
+
+			if (timeSpan.TotalDays >= 30)
 			{
 				int months = (int)(timeSpan.TotalDays / 30);
-				return $"Прошло {months} {GetPluralForm(months, "месяц", "месяца", "месяцев")}";
+				return $"{months} мес.";
 			}
 			else if (timeSpan.TotalDays >= 7)
 			{
 				int weeks = (int)(timeSpan.TotalDays / 7);
-				return $"Прошло {weeks} {GetPluralForm(weeks, "неделя", "недели", "недель")}";
+				return $"{weeks} нед.";
 			}
 			else if (timeSpan.TotalDays >= 1)
 			{
 				int days = (int)timeSpan.TotalDays;
-				return $"Прошло {days} {GetPluralForm(days, "день", "дня", "дней")}";
+				return $"{days} дн.";
 			}
 			else if (timeSpan.TotalHours >= 1)
 			{
 				int hours = (int)timeSpan.TotalHours;
-				return $"Прошло {hours} {GetPluralForm(hours, "час", "часа", "часов")}";
+				return $"{hours} час.";
 			}
 			else if (timeSpan.TotalMinutes >= 1)
 			{
 				int minutes = (int)timeSpan.TotalMinutes;
-				return $"Прошло {minutes} {GetPluralForm(minutes, "минута", "минуты", "минут")}";
+				return $"{minutes} мин.";
 			}
 			else
 			{
 				int seconds = (int)timeSpan.TotalSeconds;
-				return $"Прошло {seconds} {GetPluralForm(seconds, "секунда", "секунды", "секунд")}";
-			}
-		}
-
-		private static string GetPluralForm(int value, string singular, string few, string many)
-		{
-			if (value % 10 == 1 && value % 100 != 11)
-			{
-				return singular;
-			}
-			else if (value % 10 >= 2 && value % 10 <= 4 && (value % 100 < 10 || value % 100 >= 20))
-			{
-				return few;
-			}
-			else
-			{
-				return many;
+				return $"{seconds} сек.";
 			}
 		}
 	}
