fa82ae1b291a214fa67e5ec7f46dd1d48022e549
Danil Pryahin <pradanilhin@gmail.com>
2024-10-24 12:22:20 +0700

    made coockieManager more clear and also added support for Map and Array

--- EMAS_Web/wwwroot/js/coockieManager.js
+++ EMAS_Web/wwwroot/js/coockieManager.js
@@ -6,34 +6,44 @@ function setCookie(name, value, days) {
 
         if (value instanceof Map) {
             value = JSON.stringify(Array.from(value.entries()));
+            value = "Map:" + value;
         } else if (typeof value === 'object') {
             value = JSON.stringify(value);
         }
 
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + "=" + encodeURIComponent(value || "") + expires + "; path=/";
 }
 
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
-        const jsonValue = decodeURIComponent(parts.pop().split(';').shift());
+        let jsonValue = decodeURIComponent(parts.pop().split(';').shift());
 
         try {
-            const parsedValue = JSON.parse(jsonValue);
-            // Если это массив, преобразуем его обратно в Map
-            if (Array.isArray(parsedValue)) {
-                return new Map(parsedValue); // Преобразуем массив обратно в Map
+            if (jsonValue.length >= 4 && jsonValue.substring(0, 4) === "Map:") {
+                jsonValue = jsonValue.substring("Map:".length);
+                let parsedValue = JSON.parse(jsonValue);
+                return new Map(parsedValue);
             }
-            return parsedValue; // Возвращаем обычный объект
+
+            let parsedValue = JSON.parse(jsonValue);
+
+            if (Array.isArray(parsedValue)) {
+                return parsedValue;
+            }
+
+            return parsedValue;
+
         } catch (error) {
-            console.error('Ошибка при парсинге cookie:', error);
+            console.error('РћС€РёР±РєР° РїСЂРё РїР°СЂСЃРёРЅРіРµ cookie:', error);
         }
     }
-    return null; // Возвращаем null, если cookie не найдено
+    return null;
 }
+
 
 
 function deleteCookie(name) {
